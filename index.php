0fca3686230f22cd4f5a5405ae8914caab0f8b66
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-08 11:41:46 -0700

    New Instagram Project Video 2

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+//Configuration for our PHP Server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//Make Constants using define.
+define('client_id', '9fef050485a24a29b5b58022c04b2257');
+define('client_secret', '7d72e590ed0e42a1b1a3f29e73627e38');
+define('redirectURL', 'http://localhost:/Kenneth/index.php');
+define('ImageDirectory', 'pics/');
+?>
+
+
+<!-- CLIENT INFO
+CLIENT ID 9fef050485a24a29b5b58022c04b2257
+CLIENT SECRET 7d72e590ed0e42a1b1a3f29e73627e38
+WEBSITE URL http://localhost:/Kenneth/index.php
+REDIRECT URI http://localhost:/Kenneth/index.php -->
