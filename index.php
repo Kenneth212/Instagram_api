609ed94d15bcd31472a1d219a9cc5630eb4b8a56
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-13 11:41:53 -0700

    Instagram fixed the errors.

--- index.php
+++ index.php
@@ -1,31 +1,28 @@
 <?php
+
 //Configuration for our PHP Server
 set_time_limit(0);
 ini_set('default_socket_timeout', 300);
+
 session_start();
 
-//Make Constants using define.
-define('client_ID', '9fef050485a24a29b5b58022c04b2257');
-define('client_Secret', '7d72e590ed0e42a1b1a3f29e73627e38');
-define('redirectURL', 'http://localhost:/Kenneth/index.php');
+//Make Constant using define.
+define('clientID', '49d565177a2f408a9988bdee6220cc46');
+define('clientSecret', 'c793f86523d441219037400b93598a54');
+define('redirectURI', 'http://localhost/Kenneth/index.php');
 define('ImageDirectory', 'pics/');
+
+
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="description" contents="">
-		<meta name="viewport" content="width=device=width, intial=scale=1">
-		<title>Untitled</title>
-		<link rel="stylesheet" href="css/style.css">
-		<link rel="author" href="humans.txt">
-	</head>
-	<body>
-		<!-- Creating a login for people to go and give approval for our web app to access their Instagram Account
-		After getting approval we are now going to have the informationso that we can play with it.
-		 -->
-		<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_url=<?php echo redirectURL?>&response_type=code">LOGIN</a>
-		<script type="js/main.js"></script>
-	</body>	
-</html
+<head>
+	<title></title>
+</head>
+<body>
+	
+	<a href="https://api.instagram.com/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">Login</a>
+	
+</body>
+</html>
