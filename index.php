2f6b49da0df83489032331670443fd7c47f77caf
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-14 11:56:59 -0700

    Instagram Milestone 10.

--- index.php
+++ index.php
@@ -10,6 +10,23 @@ define('clientSecret', 'c793f86523d441219037400b93598a54');
 define('redirectURI', 'http://localhost/Kenneth/index.php');
 define('ImageDirectory', 'pics/');
 
+//function that is going to connect to Instagram.
+function connectToInstagram($url){
+	$ch = curl_init();
+
+	curl_setopt_array($ch, array(
+		CURLOPT_URL =>$url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => 2,
+	));
+	$result = curl_exec($ch);
+	curl_close($ch);
+	return $result;
+
+}
+
+
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'http://api.instagram.com/oauth/access_token';
