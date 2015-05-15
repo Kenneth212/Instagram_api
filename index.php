ff1cc1ea61fe794ea9de76d75be39da567a7c03c
Kenneth Prado <Kennethprado45@gmail.com>
2015-05-14 17:54:42 -0700

    Instagram Milestone 13.

--- index.php
+++ index.php
@@ -41,7 +41,18 @@ function printImages($userID){
 	foreach ($results['data'] as $items){
 		$image_url = $items['images']['low_resolution']['url'];//going to go through all of my results and give myself back the url of those pictures because we want to save it in the PHP server
 		echo '<img src=" ' .$image_url. ' "/><br/>';
+		//calling a function to save that $image_url
+		savePictures($url_url);
 	}
+}
+//function to save image to server
+function savePictures($image_url){
+	echo $image_url.'<br>';
+	$filename = basename($image_url);//the filename is what we are storing, basename is the PHP built in method that we are using to store $image_url
+	echo $filename . '<br>';
+
+	$destination = ImageDirectory . $filename;//making sure that the image doesn't exist in the storage.
+	file_put_contents($destination, file_get_contents($image_url));//goes and grabs an imagefile and stores it into our server
 }
 
 
